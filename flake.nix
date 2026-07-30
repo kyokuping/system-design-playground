7b3ea52c2b96facc73562775d1fe4b6bdefb9043
kyokuping <me@kyoku.dev>
2026-07-30 19:20:53 +0900

    chore: set up Go development environment with Nix

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Go playground for system design interview exercises";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              gopls
+              delve
+              gotools
+              golangci-lint
+              go-tools
+            ];
+
+            shellHook = ''
+              echo "Go $(go version | cut -d' ' -f3) development shell"
+            '';
+          };
+        }
+      );
+
+      formatter = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.nixfmt
+      );
+    };
+}
